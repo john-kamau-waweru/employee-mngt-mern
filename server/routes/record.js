fb667a986f934f20dbc8ad743a162f273e6ee9fe
root <root@NDK205-JohnWaweru-win.localdomain>
2026-03-14 14:29:08 +0300

    added patch and delete methods to the server

--- server/routes/record.js
+++ server/routes/record.js
@@ -37,3 +37,37 @@ router.post("/", async (req, res) => {
         res.status(500).send("Error adding record")
     }
 });
+
+// Update a record
+router.patch("/:id", async (req, res) => {
+    try {
+        const query = { _id: new ObjectId(req.params.id) };
+        const updates = {
+            $set: {
+                name: req.body.name,
+                position: req.body.position,
+                level: req.body.level,
+            }
+        };
+        let collection = await db.collection("records");
+        let result = await collection.updateOne(query, updates);
+        res.send(result).status(200);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error updating record");
+    }
+});
+
+// Delete a record
+router.delete("/:id", async (req, res) => {
+    try {
+        const query = { _id: new ObjectId(req.params.id) };
+
+        const collection = await db.collection("records");
+        let result = await collection.deleteOne(query);
+        res.send(result).status(200);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error deleting record");
+    }
+});
